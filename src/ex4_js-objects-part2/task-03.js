4aa3b1d0f37c44a17a2b85e44215170f3f096321
markmarkberg <mark7.0@inbox.ru>
2020-10-18 19:08:34 +0300

    Fixed task-03

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,5 +1,16 @@
 const getStringWithoutSpace = (string) => {
-  return string.slice(1, -1);
+  let modifiedString = string;
+
+  if (modifiedString[0] === " ") {
+    modifiedString = modifiedString.slice(1);
+  }
+
+  if (string[length] === " ") {
+    modifiedString = modifiedString.slice(0, -1);
+  }
+
+  return modifiedString;
 };
 
 module.exports = getStringWithoutSpace;
+
