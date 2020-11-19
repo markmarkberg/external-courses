41e797fcbdbed7a0e7febedb53f789b771734abe
markmarkberg <mark7.0@inbox.ru>
2020-11-19 22:33:45 +0300

    Fixed kanban

--- src/kanban/script.js
+++ src/kanban/script.js
@@ -1,45 +1,43 @@
-let index = false;
+const button = document.querySelector('.account');
+button.setAttribute('data-index', 'false');
 
-const button = document.querySelector(".account");
+const userName = document.createElement('p');
+userName.classList.add('user_name');
+userName.innerHTML = 'Mark';
 
-const userName = document.createElement("p");
-userName.classList.add("user_name");
-userName.innerHTML = "Mark";
+const account = document.createElement('button');
+account.classList.add('dropdown_list_item');
+account.innerHTML = 'My account';
 
-const account = document.createElement("button");
-account.classList.add("dropdown_list_item");
-account.innerHTML = "My account";
+const tasks = document.createElement('button');
+tasks.classList.add('dropdown_list_item');
+tasks.innerHTML = 'My tasks';
 
-const tasks = document.createElement("button");
-tasks.classList.add("dropdown_list_item");
-tasks.innerHTML = "My tasks";
+const exit = document.createElement('button');
+exit.classList.add('dropdown_list_item');
+exit.innerHTML = 'Log out';
 
-const exit = document.createElement("button");
-exit.classList.add("dropdown_list_item");
-exit.innerHTML = "Log out";
+const div = document.createElement('div');
+div.classList.add('dropdown_list_active');
 
-const div = document.createElement("div");
-div.classList.add("dropdown_list_active");
+div.append(userName, account, tasks, exit);
 
 const listActive = () => {
-  document.querySelector(".dropdown").classList.add("dropdown_logo_rotate");
-  document.querySelector(".dropdown_container").append(div);
-  document
-    .querySelector(".dropdown_list_active")
-    .append(userName, account, tasks, exit);
+  document.querySelector('.dropdown').classList.add('dropdown_logo_rotate');
+  document.querySelector('.dropdown_container').append(div);
 };
 
 const removeDropList = () => {
-  document.querySelector(".dropdown_list_active").remove();
-  document.querySelector(".dropdown").classList.remove("dropdown_logo_rotate");
+  document.querySelector('.dropdown_list_active').remove();
+  document.querySelector('.dropdown').classList.remove('dropdown_logo_rotate');
 };
 
-button.addEventListener("click", () => {
-  if (index === false) {
+button.addEventListener('click', () => {
+  if (button.dataset.index === 'false') {
     listActive();
-    index = true;
+    button.dataset.index = 'true';
   } else {
     removeDropList();
-    index = false;
+    button.dataset.index = 'false';
   }
 });
