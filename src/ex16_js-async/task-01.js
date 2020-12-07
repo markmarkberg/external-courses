b003cc84ce4273b83de753677682b093ed5f4331
markmarkberg <mark7.0@inbox.ru>
2020-12-07 20:01:10 +0300

    Add task-01

---
+++ src/ex16_js-async/task-01.js
@@ -0,0 +1,25 @@
+const processRequest = function (
+  url,
+  methodRequest = "GET",
+  bodyRequest = null
+) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(methodRequest, url, true);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status <= 299) {
+        resolve(this.response);
+      } else {
+        let error = new Error(this.statusText);
+        error.code = this.status;
+        reject(error);
+      }
+    };
+    xhr.onerror = function () {
+      reject(new Error("Ошибка соединения"));
+    };
+    xhr.send(bodyRequest);
+  });
+};
+
+module.exports = processRequest;
